ea057fff9ab4d3070559c2a68553b5cc46c1ac61
조형근 <jhkluck22@gmail.com>
2023-11-27 21:13:36 +0900

    11/27

--- chapter3/main2-3.py
+++ chapter3/main2-3.py
@@ -1,4 +1,13 @@
-import requests
-import re
+a = 'a'
+b = 'b'
 
-req
+while True:
+    a = input("영어 단어를 입력하세요: ")
+    if a == 'q':
+        break
+    b = input("한국어 뜻을 입력하세요: ")
+    if b == 'q':
+        break
+    with open('vocabulary.txt', 'a') as f:
+        f.write("{}: ".format(a))
+        f.write("{}\n".format(b))
